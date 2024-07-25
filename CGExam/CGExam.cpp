c129fbdba672f4d0420fb6d3b765534f3a1c3b35
AlexRod-Dev <alexandre-tavares@hotmail.com>
2024-07-25 19:00:24 +0100

    Project Creation and dependencies defined

---
+++ CGExam/CGExam.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <SDL.h>
+#include "stb_image.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <vector>
+#include <string>
+
+int main(int argc, char* args[])
+{
+	// Your code here
+
+	
+	return 0;
+
+
+}
